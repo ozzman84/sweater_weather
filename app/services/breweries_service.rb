afd2696b3750e56d1994a172ce2206e67e5d33e5
Ozzie <mikeosmonson@gmail.com>
2021-11-16 11:15:14 -0700

    Checkin

--- app/services/breweries_service.rb
+++ app/services/breweries_service.rb
@@ -1,10 +1,33 @@
 class BreweriesService
   class << self
     def find_brewereies(weather, params)
-      request = BreweriesClient.request_+breweries("/breweries?by_city=#{params[:location].split(',')[-2].strip}")
-      breweries = request.each do |brewery|
+      request = BreweriesClient.request_breweries("/breweries?by_city=#{params[:location].split(',')[-2].strip}")
+      breweries = request[1..params[:quantity].to_i].map do |brewery|
         BreweryPoro.new(brewery)
       end
+
+      {
+        "data": {
+          "id": nil,
+          "type": "breweries",
+          "attributes": {
+            "destination": params[:location],
+            "forecast": {
+              "summary": weather[:data][:attributes][:current_weather][:conditions],
+              "temperature": "#{weather[:data][:attributes][:current_weather][:temperature]} F"
+            },
+            "breweries": breweries
+          }
+        }
+      }
     end
+
+    # private
+    #
+    # def form_data(weather, params)
+    #   {
+    #
+    #   }
+    # end
   end
 end
