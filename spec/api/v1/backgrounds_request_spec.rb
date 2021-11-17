2b77afd03c119e1f31585d9ebf01a100f1527088
Ozzie <mikeosmonson@gmail.com>
2021-11-17 15:48:26 -0700

    Error testing

--- spec/api/v1/backgrounds_request_spec.rb
+++ spec/api/v1/backgrounds_request_spec.rb
@@ -3,9 +3,13 @@ require 'rails_helper'
 describe 'Backgrounds Request' do
   describe 'valid location' do
     let(:location) { 'denver,co' }
+    # before { get '/api/vi/backgrounds', params: {location: 'denver,co' } }
 
     it 'response is successful' do
-      get(api_v1_backgrounds(location: "denver,co"))
+      # get(api_v1_backgrounds(location: "denver,co"))
+      # get '/api/vi/backgrounds?location=denver,co'
+      get '/api/vi/backgrounds', params: {location: 'denver,co'}
+
       expect(response).to be_successful
     end
   end
